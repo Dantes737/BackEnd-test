2a32ec9393f5a92873d6cea6d656bc3a8fbefa82
Dantes737 <you@example.com>
2021-04-07 21:01:57 +0300

    gggg

--- config/dataBase.js
+++ config/dataBase.js
@@ -1,12 +1,12 @@
-const { Sequelize} = require("sequelize");
+const { Sequelize } = require("sequelize");
+require('dotenv').config();
 
-// Option 2: Passing parameters separately (other dialects)
-// const sequelize = new Sequelize('database', 'username', 'password', {
-//     host: 'localhost',
-//     dialect:'postgres'
-//   });
-module.exports= new Sequelize('my_shop_db', 'postgres', 'p0tat0888', {
-    host: 'localhost',
-    dialect: 'postgres',
-    operatorsAliases: false,
-});
+module.exports = new Sequelize(
+    process.env.DATA_BASE_NAME,
+    process.env.USER_NAME,
+    process.env.DB_ACCES_PASSWORD,
+    {
+        host: process.env.HOST,
+        dialect: process.env.DB_DIALECT,
+        operatorsAliases: false,
+    });
